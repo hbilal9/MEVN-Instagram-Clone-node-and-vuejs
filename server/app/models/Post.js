3b9d2c2c2f0808ab35774cd6d62d9c07730f9c29
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2021-12-05 11:17:41 +0500

    Post Schema added

--- server/app/models/Post.js
+++ server/app/models/Post.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {ObjectId} = mongoose.Schema;
+const {ObjectId} = mongoose.Schema.Types.ObjectId;
 
 const postSchema = new mongoose.Schema({
     title: {
